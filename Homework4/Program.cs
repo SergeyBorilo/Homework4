87eb5d13c997d91be1a59e794388f97b1ed0751c
unknown <s_barya@i.ua>
2024-12-17 20:22:40 +0200

    Homework4 commit

---
+++ Homework4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var collection = new TemplateCollection<int>();
+
+        collection.OnExpandedEvent += () => Console.WriteLine("Array expansion");
+
+        collection.Add(5);
+        collection.Add(8);
+        collection.Add(15);
+        collection.Add(2);
+        collection.Add(12);
+        collection.Add(11);
+
+        var sorted = collection.OrderBy(x => x).ToList();
+        var filtered = collection.Where(x => x > 8).ToList();
+
+        Console.WriteLine("Collection:");
+        foreach (var element in collection) Console.WriteLine(element);
+
+        Console.WriteLine("sSorted collection:");
+        foreach (var element in sorted) Console.WriteLine(element);
+
+        Console.WriteLine("Filtered collection:");
+        foreach (var element in filtered) Console.WriteLine(element);
+    }
+}
---
+++ Homework4/TemplateCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework4;
+
+public class TemplateCollection<T> : IEnumerable<T>
+{
+    private T[] _data;
+
+    public event Action OnExpandedEvent;
+
+    public TemplateCollection()
+    {
+        _data = [];
+
+    }
+
+    public T this[int index]
+    {
+        get => _data[index];
+        set => _data[index] = value;
+    }
+
+    public void Add(T element)
+    {
+        Array.Resize(ref _data, _data.Length + 1);
+        _data[^1] = element;
+        OnExpandedEvent?.Invoke();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var element in _data) yield return element;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
